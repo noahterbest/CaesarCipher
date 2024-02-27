706e707948cffda77b66dd0cbfbf9aa62dd55b3b
noahterbest <noahterbest@gmail.com>
2024-02-27 16:37:11 -0700

    added logic with -3 encryption key.

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,29 +1,51 @@
 // Noah TerBest
 // CSC 1060
 
+import java.util.Scanner;
+
 public class Cipher {
     public static void main(String[] args) {
+        //Parallel Arrays
+        char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-        // Parallel arrays
-        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        // Wasn't sure how to make it possible for the user to choose an encryption like your example, is that possible
+        // with parallel arrays?
 
-        char[] shifted = {'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c'};
+        char cipher[] = {'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k','l', 'm', 'n', 'o', 'p', 'q','r', 's' , 't', 'u', 'v', 'w'};
 
-        // Variables
-        String plaintext = "hello how are you";
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for message
+        System.out.print("Enter message to encrypt: ");
+        String plaintext = scanner.nextLine();
         String ciphertext = "";
 
-        // Loop through plaintext
+
+        // Encrypt message
         for (int i = 0; i < plaintext.length(); i++) {
-            char ch = plaintext.charAt(i);
-            int index = ch - 'a';
-            char shiftedChar = shifted[index];
-            ciphertext += shiftedChar;
+
+            char letter = plaintext.charAt(i);
+            int index = -1;
+
+            for (int j = 0; j < alphabet.length; j++) {
+                // Find index of letter in alphabet
+                if (letter == alphabet[j]) {
+                    index = j;
+                }
+            }
+            // Check if letter is in alphabet
+            if (index!= -1) {
+                // Encrypt letter
+                ciphertext += cipher[index];
+            }else {
+                // If letter is not in alphabet, leave it
+                ciphertext += letter;
+            }
         }
 
+        //Print ciphertext message
         System.out.println(ciphertext);
-
     }
 }
